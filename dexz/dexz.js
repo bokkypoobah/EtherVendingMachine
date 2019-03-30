fbf7712a3a25a16e6251e9f8907b81d90caec5a1
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2019-03-31 10:41:58 +1100

    Dexz WIP

--- dexz/dexz.js
+++ dexz/dexz.js
@@ -8,20 +8,20 @@ const Dexz = {
       name: "Dexz",
     }
   },
-  beforeRouteEnter(to, from, next) {
-    console.log("[dexz.beforeRouteEnter] - to: " + to + ", from: " + from + " , next: " + next);
-    next(vm => {
-      // access to component's instance using `vm` .
-      // this is done because this navigation guard is called before the component is created.
-      // clear your previously populated search results.
-      // re-populate search results
-      console.log("[dexz.tokenContractFactory -> next] vm: ");
-      console.table(vm);
-      // vm.initializeSearch();
-      next();
-    })
-  },
-  processEth() {
-    console.log("[dexz] processEth()");
-  }
+  // beforeRouteEnter(to, from, next) {
+  //   console.log("[dexz.beforeRouteEnter] - to: " + to + ", from: " + from + " , next: " + next);
+  //   next(vm => {
+  //     // access to component's instance using `vm` .
+  //     // this is done because this navigation guard is called before the component is created.
+  //     // clear your previously populated search results.
+  //     // re-populate search results
+  //     console.log("[dexz.tokenContractFactory -> next] vm: ");
+  //     console.table(vm);
+  //     // vm.initializeSearch();
+  //     next();
+  //   })
+  // },
+  // processEth() {
+  //   console.log("[dexz] processEth()");
+  // }
 };
