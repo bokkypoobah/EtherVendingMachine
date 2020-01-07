6a73ff576f1d9230e07c102ffdc605c3d91320d1
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2020-01-07 15:25:57 +1100

    Clearing out old stuff

--- vuex/functions.js
+++
@@ -1,7 +0,0 @@
-function formatNumber(n) {
-    return n == null ? "" : n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function logIt(s, t) {
-  console.log(new Date().toLocaleTimeString() + " " + s + ":" + t);
-}
